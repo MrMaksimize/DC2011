663431e7362956df640d2b0f2a963da30d0aec7f
Erik Baldwin <ThatGuy@ErikBaldwin.com>
2011-10-06 01:33:13 -0500

    ThatGuy: Finally got all of the forms and buttons looking decent. also got the form process working for registration. we can reuse the process, but the progress bar doesn't really accommodate 5 or more steps.

--- sites/all/themes/chicago_2011/js/formToWizard.js
+++ sites/all/themes/chicago_2011/js/formToWizard.js
@@ -1,72 +1,79 @@
 
 (function($) {
-    $.fn.formToWizard = function(options) {
-        options = $.extend({  
-            submitButton: "" 
-        }, options); 
-        
-        var element = this;
+	$.fn.formToWizard = function(options) {
+		options = $.extend({  
+			submitButton: "",
+			nextLabel: "Next >",
+			prevLabel: "< Back" 
+		}, options); 
+		
+		var element = this;
 
-        var steps = $(element).find("fieldset");
-        var count = steps.size();
-        var submmitButtonName = "#" + options.submitButton;
-        $(submmitButtonName).hide();
+		var steps = $(element).find("fieldset");
+		var count = steps.size();
+		var submmitButtonName = "#" + options.submitButton;
+		
+		$(submmitButtonName).hide();
 
-        $(element).before("<ul id='steps'></ul>");
+		$(element).before("<ul id='steps'></ul>");
 
-        steps.each(function(i) {
-            $(this).wrap("<div id='step" + i + "' class='step'></div>");
-            $(this).append("<p id='step" + i + "buttons' class='buttons'></p>");
+		steps.each(function(i) {
+			$(this).wrap("<div id='step" + i + "' class='step'></div>");
+			$(this).append("<p id='step" + i + "buttons' class='buttons'></p>");
 
-            var name = $(this).find("legend").html();
-            $("#steps").append("<li id='stepDesc" + i + "' class='' style='z-index: " + (count - i) + ";'><span class='stepNum'>" + (i + 1) + "</span><span class='stepName'>" + name + "</span></li>");
+			var name = $(this).find("legend").html();
+			$("#steps").append("<li id='stepDesc" + i + "' class='' style='z-index: " + (count - i) + ";'><span class='stepNum'>" + (i + 1) + "</span><span class='stepName'>" + name + "</span></li>");
 
-            if (i == 0) {
-                createNextButton(i);
-                selectStep(i);
-            }
-            else if (i == count - 1) {
-                $("#step" + i).hide();
-                createPrevButton(i);
-            }
-            else {
-                $("#step" + i).hide();
-                createPrevButton(i);
-                createNextButton(i);
-            }
-        });
+			if (i == 0) {
+				createNextButton(i);
+				selectStep(i);
+			}
+			else if (i == count - 1) {
+				$("#step" + i).hide();
+				createPrevButton(i);
+			}
+			else {
+				$("#step" + i).hide();
+				createPrevButton(i);
+				createNextButton(i);
+			}
+		});
+		
+		// Find the last LI generated and add .last to it
+		var stepItems = $('#steps li');
+		stepItems.eq(stepItems.length - 1).addClass('last');
 
-        function createPrevButton(i) {
-            var stepName = "step" + i;
-            $("#" + stepName + "buttons").append("<a href='#stepDesc" + (i - 1) + "' id='" + stepName + "Prev' class='prev'>< Back</a>");
+		function createPrevButton(i) {
+			var stepName = "step" + i;
+			$("#" + stepName + "buttons").append("<a href='#stepDesc" + (i - 1) + "' id='" + stepName + "Prev' class='button prev'>" + options.prevLabel + "</a>");
 
-            $("#" + stepName + "Prev").bind("click", function(e) {
-                $("#" + stepName).hide();
-                $("#step" + (i - 1)).show();
-                $(submmitButtonName).hide();
-                selectStep(i - 1);
-            });
-        }
+			$("#" + stepName + "Prev").bind("click", function(e) {
+				$("#" + stepName).hide();
+				$("#step" + (i - 1)).show();
+				$(submmitButtonName).hide();
+				selectStep(i - 1);
+			});
+		}
 
-        function createNextButton(i) {
-            var stepName = "step" + i;
-            $("#" + stepName + "buttons").append("<a href='#stepDesc" + (i + 1) + "' id='" + stepName + "Next' class='next'>Next ></a>");
+		function createNextButton(i) {
+			var stepName = "step" + i;
+			$("#" + stepName + "buttons").append("<a href='#stepDesc" + (i + 1) + "' id='" + stepName + "Next' class='button next'>" + options.nextLabel + "</a>");
 
-            $("#" + stepName + "Next").bind("click", function(e) {
-                $("#" + stepName).hide();
-                $("#step" + (i + 1)).show();
-                if (i + 2 == count)
-                    $(submmitButtonName).show();
-                selectStep(i + 1);
-            });
-        }
+			$("#" + stepName + "Next").bind("click", function(e) {
+				$("#" + stepName).hide();
+				$("#step" + (i + 1)).show();
+				if (i + 2 == count)
+					$(submmitButtonName).show();
+				selectStep(i + 1);
+			});
+		}
 
-        function selectStep(i) {
-            $("#steps li").removeClass("current");
-            $("#stepDesc" + i).addClass("current");
-						$("#stepDesc" + (i - 1)).addClass("completed");
-						$("#stepDesc" + i).removeClass("completed");
-        }
+		function selectStep(i) {
+			$("#steps li").removeClass("current");
+			$("#stepDesc" + i).addClass("current");
+			$("#stepDesc" + (i - 1)).addClass("completed");
+			$("#stepDesc" + i).removeClass("completed");
+		}
 
-    }
+	}
 })(jQuery); 
--- sites/all/themes/chicago_2011/js/theme-script.js
+++ sites/all/themes/chicago_2011/js/theme-script.js
@@ -6,14 +6,21 @@ $(document).ready(function() {
  */
 	if (jQuery('#user-register').length > 0) {
     var mollom = jQuery('#edit-mollom-captcha-wrapper').clone();
+		//var formSubmit = jQuery('#user-register #edit-submit').clone();
     jQuery('#edit-mollom-captcha-wrapper').remove();
     jQuery('#edit-mail-wrapper').parent('fieldset').append(mollom);
+		//jQuery('').child('').append(formSubmit);
   };
 
 /**
  * Add steps to forms with fieldsets
+ *
+ * Options available to be passed to "formToWizard({ options.FOO })" and their default values: 
+            submitButton: "" //add the ID of the form-submit button, WITHOUT the "#", it is automatically added. DEFAULT == NULL
+						prevLabel: "" // DEFAULT == < Back
+						nextLabel: "" //DEFAULT == Next >
  */
-	jQuery("#user-register").formToWizard({ submitButton: 'edit-submit' });
+	jQuery("#user-register").formToWizard({ submitButton: 'edit-submit', prevLabel: '< Previous', nextLabel: 'Proceed >' });
 	 
 });
 //}(jQuery);
